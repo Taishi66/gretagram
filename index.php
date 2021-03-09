c69afd12c54d4baa876efb601dbb2c991dff6f7a
Taishi <jctlamy@gmail.com>
2021-03-09 15:33:47 +0100

    up

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
-require_once './vendor/autoload.php';
- 
-// application code
+// demarre une session
+session_start();
 
-use api\Instagram;
+require_once("bdd/bdd.php");
+include("view/header.php");
 
-require "Instagram.php";
-testestest
+include("controller/profilsController.php");
+
+require_once("Router.php");
+
+$router = new Router(@$_GET["page"]); // On récupère la valeur associée à la clé "page" dans l'url
+// exemple localhost/index.php?page=profils 
+$router->getPage();
+
+
+include("view/footer.php");
