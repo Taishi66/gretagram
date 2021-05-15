512d13173b498bbae10491a959bb1c7fd9ccd052
Taishi <jctlamy@gmail.com>
2021-05-15 22:16:16 +0200

    fix bdd

--- core/Bdd.php
+++ core/Bdd.php
@@ -9,7 +9,7 @@ class Bdd
 
     //  methode static
     public static function Connexion()
-    {
+    { //Si n'est pas connecté alors il se connectera
         if (self::$bdd === null) {
             try {
                 $bdd = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_DATABASE'], $_ENV['DB_PASSWORD'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
@@ -19,7 +19,7 @@ class Bdd
                 die('erreur de connexion à la bdd <br> $e');
             }
         }
-
+        //Si déjà connecté alors il reprendra cette connexion
         return self::$bdd;
     }
 }
