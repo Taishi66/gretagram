fa9e6c71c88ded8599469e269767e633da05144d
Taishi <jctlamy@gmail.com>
2021-05-18 21:23:36 +0200

    test esat

--- esat/index.php
+++ esat/index.php
@@ -9,21 +9,29 @@
 </head>
 
 <body>
+    <?php
+    include("vendor/autoload.php");
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    ?>
+
     <?php include('controller.php'); ?>
-    <form method="post" action="">
-        <input type="radio" name="sexe">Homme
-        <input type="radio" name="sexe">femme
-        <input name="nom" id="nom" placeholder="entrez votre nom">
-        <input name="prenom" id="prenom" placeholder="entrez votre prenom">
-        <select name="ville" id="ville">
-            <option>Paris</option>
-            <option>Lyon</option>
-        </select>
-        <textarea name="description" id="description"></textarea>
-        <input name="email" id="email" placeholder="mail">
-        <input name="mdp" id="mdp" placeholder="mdp">
-        <button type="submit" name="submit">envoyer</button>
-    </form>
+    <center>
+        <form method="post" action="">
+            <input type="radio" name="sexe">Homme<br>
+            <input type="radio" name="sexe">femme<br>
+            <input name="nom" id="nom" placeholder="entrez votre nom"><br><br>
+            <input name="prenom" id="prenom" placeholder="entrez votre prenom"><br><br>
+            <select name="ville" id="ville"><br><br>
+                <option>Paris</option>
+                <option>Lyon</option>
+            </select><br><br>
+            <textarea name="description" id="description"></textarea><br><br>
+            <input name="email" id="email" placeholder="mail"><br><br>
+            <input name="mdp" id="mdp" placeholder="mdp"><br><br>
+            <button type="submit" name="submit">envoyer</button>
+        </form>
+    </center>
     <?php $controller = new Controller();
     $controller->ajout();
     ?>
